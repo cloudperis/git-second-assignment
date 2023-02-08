5ecb3b61802046c292ea1c97dd29829f3834b298
Olubode Job <bodeige81@gmail.com>
2023-02-07 23:07:55 -0600

    Updated by Olu

---
+++ BODE-SCRIPT.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# mv command:    "To rename or move a file"
+# echo command:  "To add some content line in the new file
+# grep command:  "To filter the content of the file
+# mkdir command: "To create a directory
+# cat  command:  "To output content of a file
+
