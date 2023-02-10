23b2e700ff9d19496361d3628c19c97320ba3407
lekzy2088 <lekzy2088@gmail.com>
2023-02-10 18:34:40 -0500

    my script

---
+++ LEKE-SCRIPT.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+# create a file
+touch created-file.txt
+
+# add content to file
+echo "I am a DevOps Engineer" > created-file.txt
+
+# change file name
+mv created-file.txt changed-file.txt
